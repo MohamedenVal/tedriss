67c0412d1b39cd7aea33eb7560d372cd48b53297
MohamedenVal <med4en@gmail.com>
2021-01-21 02:21:41 +0000

    added the course page with some functionalities

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// variables declaration 
+
+const dropDown = document.querySelector(".circle-content");
+
+// Event listeners
+
+dropDown.addEventListener("click", showContent);
+
+
+// function declaration 
+
+function showContent(event){
+    /* this function will make any information about the title show up under the label */
+
+    let element = document.querySelector(".hcj-content");
+    element.style = "display: block;"
+    element.classList.toggle("appear");
+}
